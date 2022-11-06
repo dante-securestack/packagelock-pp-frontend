b4275ce34dac5d602fce06abf1d0fe2aa4a2366d
Igor Trindade <igortrindademe@gmail.com>
2022-11-06 16:27:40 -0300

    feat: Installed vercel audience analytics

--- plugins/general.client.ts
+++ plugins/general.client.ts
@@ -4,9 +4,12 @@ import setupComponents from '@/setup/components'
 import setupGlobals from '@/setup/globals'
 import setupRouterHooks from '@/setup/routerHooks'
 
+import { inject as initVercelAnalytics } from '@vercel/analytics'
+
 export default defineNuxtPlugin((nuxtApp) => {
   setupDirectives(nuxtApp.vueApp)
   setupComponents(nuxtApp.vueApp)
   setupGlobals(nuxtApp.vueApp)
   setupRouterHooks()
+  initVercelAnalytics()
 })
