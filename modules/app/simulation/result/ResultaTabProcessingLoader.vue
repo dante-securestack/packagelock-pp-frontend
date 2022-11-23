0a47bd3252ef7e3792482371f8d1a6949a36ab99
Igor Trindade <igortrindademe@gmail.com>
2022-11-23 20:52:09 -0300

    feat: Adicionado frases dinamicas no loader de processamento

--- modules/app/simulation/result/ResultaTabProcessingLoader.vue
+++ modules/app/simulation/result/ResultaTabProcessingLoader.vue
@@ -1,16 +1,21 @@
 <template>
   <div class="w-full flex flex-wrap p-8 shadow-md bg-cyan-800/5">
-    <div class="w-full lg:w-1/3 p-6 flex flex-col justify-center space-y-4">
-      <div class="w-full flex">
+    <div class="w-full lg:w-1/3 p-6 flex flex-col justify-start space-y-4">
+      <div class="w-full flex mt-4">
         <h3 class="h3 text-left">Processando</h3>
-        <div class="ml-4 flex items-center space-x-2 animate-pulse">
-          <div class="w-3 h-3 bg-gray-500 rounded-full"></div>
-          <div class="w-3 h-3 bg-gray-500 rounded-full"></div>
-          <div class="w-3 h-3 bg-gray-500 rounded-full"></div>
-      </div>
+        
       </div>
       <p class="leading-relaxed">Aguarde enquanto processamos o resultado de sua aposentadoria.</p>
-      <p class="leading-relaxed">Não saia desta tela, o resultado da sua aposentadoria irá aparecer em instantes.</p>
+      <Transition name="fade">
+        <p class="leading-relaxed" v-if="currentPhrase > -1">
+          {{ phrases[currentPhrase] }}
+        </p>
+      </Transition>
+      <span class="flex items-center space-x-2 animate-pulse">
+        <div class="w-3 h-3 bg-gray-500 rounded-full"></div>
+        <div class="w-3 h-3 bg-gray-500 rounded-full"></div>
+        <div class="w-3 h-3 bg-gray-500 rounded-full"></div>
+      </span>
     </div>
     <div class="w-full lg:w-2/3 flex flex-wrap items-center justify-center">
       <img class="w-full w-1/2 lg:w-3/4" src="/images/general/pack-illustrations/16.svg" />
@@ -25,5 +30,30 @@
   const props = defineProps({
     simulation: Object
   })
+
+  const currentPhrase = ref(0)
+
+  const phrases = ref([
+    `O resultado da simulação de sua aposentadoria, cálculos de tempo e projeção são elaborados com base nas suas informações prestadas e registros que conseguimos mapear do seu arquivo CNIS (data de entrada e data de saída dos vínculos).`,
+    `Para certificação final dos cálculos e simulações, consulte os nossos analistas previdenciários.`
+  ])
+
+  onMounted(() => {
+    setInterval(() => {
+      if(currentPhrase.value === phrases.value.length -1) {
+        changeCurrentPhrase(0)
+      } else {
+        changeCurrentPhrase(currentPhrase.value + 1)
+      }
+    }, 5000)
+  })
+
+
+  const changeCurrentPhrase = (current) => {
+    currentPhrase.value = -1
+    setTimeout(() => {
+      currentPhrase.value = current
+    }, 200)
+  }
 
 </script>
